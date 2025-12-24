613af77d6c546dc348e6220a000e2f3b4dd56d88
ssuzzukki <yuga.3kan@gmail.com>
2025-12-24 23:54:31 +0900

    タイトル画面の追加

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <ncurses.h>
 #include <string.h>
+#include <unistd.h>
 
+int Title(void){
+        erase();
+        mvaddstr(1, 3, "STAR CHASER");
+        mvaddstr(3, 3, "Key:");
+
+        refresh();
+        return (getch());
+}
 
 int main(void){
+	int h, w;
+	initscr();
+	noecho();
+	curs_set(0);
+	getmaxyx(stdscr, h, w);
+
+	Title();
+	endwin();
 	return 0;
 	}
