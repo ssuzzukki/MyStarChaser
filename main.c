5b750b5d4d94c4aebca1d516395327aaf29f4405
ssuzzukki <yuga.3kan@gmail.com>
2026-01-30 18:06:59 +0900

    タイトル画面の装飾

--- main.c
+++ main.c
@@ -4,16 +4,90 @@
 
 #include "game.h"
 
-int Title(void){
+int Title0(void){
+	int key;
+	timeout(-1);
+
+	while(1){
+        	refresh();
+        	erase();
+        	mvaddstr(1, 3, "    ##                                  ##                                                    ");
+        	mvaddstr(2, 3, "########                              ########        ##       ##                             ");
+        	mvaddstr(3, 3, "    ##     ##                           ##            ##        ##                            ");
+       		mvaddstr(4, 3, "  ####  ############                    ##            ##        ##      ##                    ");
+        	mvaddstr(5, 3, " #  ##     ##          ##    ##    ##   #####   ###    ##       ##  ########                  ");
+        	mvaddstr(6, 3, "  ####     ##  ###       ####  ####         ## #####   ##       ##      ##    ##    ##    ##  ");
+        	mvaddstr(7, 3, "    ##     ##                               ##   #      ###             ##      ####  ####    ");
+        	mvaddstr(8, 3, "    ##     ##                           #####   ####                 #####                    ");
+        	mvaddstr(9, 3, "           ##  ####                            #   ##               ##                        ");
+		mvaddstr(10,3, "                                                                    ##                        ");
+		mvaddstr(11,3, "                                                                     #####                    ");
+	
+        	mvaddstr(15, 3, "Star Chaser");
+        	mvaddstr(17, 3, "\"Star Chaser\" is made by KUROBA-U.");
+        	mvaddstr(19, 3, "Please enjoy \"Star Chaser\", and read \"magic of stella\"");
+        	mvaddstr(21, 3, "https://zenpo-huchui.com/magic_of_stella_game/index.php");
+        	mvaddstr(25, 3, "quit :[q]");
+
+		key = getch();
+		if(key == 'S') Title2();
+		if(key == 'q') break;
+	}
+
+        return 0;
+}
+
+int Title1(void){
         erase();
-        mvaddstr(1, 3, "Star Chaser");
-        mvaddstr(3, 3, "Key:");
-        mvaddstr(5, 3, "start:[s]");
-        mvaddstr(6, 3, "quit :[q]");
+        mvaddstr(1, 3, "    ##                                                                                                ");
+        mvaddstr(2, 3, "########                                ##    ##              ##       ##                             ");
+        mvaddstr(3, 3, "    ##     ##                            ## ###               ##        ##                            ");
+        mvaddstr(4, 3, "  ####  ############                      ##        ##        ##        ##      ##                    ");
+        mvaddstr(5, 3, " #  ##     ##          ##    ##    ##    ##       #######      ##       ##  ########                  ");
+        mvaddstr(6, 3, "  ####     ##  ###       ####  ####     ##          ## #  ###  ##       ##      ##    ##    ##    ##  ");
+        mvaddstr(7, 3, "    ##     ##                            #######    ### ## ##   ###             ##      ####  ####    ");
+        mvaddstr(8, 3, "    ##     ##                                     ######   ##                #####                    ");
+        mvaddstr(9, 3, "           ##  ####                              #  ##     ## #             ##                        ");
+	mvaddstr(10,3, "                                                    ##     ###              ##                        ");
+	mvaddstr(11,3, "                                                                             #####                    ");
+	
+        mvaddstr(15, 3, "Star Tracer");
+        mvaddstr(17, 3, "GAME start:[s]");
+        mvaddstr(19, 3, "quit :[q]");
 
         refresh();
         return (getch());
 }
+
+
+int Title2(void){
+	int key;
+        erase();
+        mvaddstr(1, 3, "    ##                                  ##                                                    ");
+        mvaddstr(2, 3, "########            # #               ########        ##       ##                             ");
+        mvaddstr(3, 3, "    ##     ##       # #                 ##            ##        ##                            ");
+        mvaddstr(4, 3, "  ####  ############                    ##            ##        ##      ##                    ");
+        mvaddstr(5, 3, " #  ##     ##          ##    ##    ##   #####   ###    ##       ##  ########                  ");
+        mvaddstr(6, 3, "  ####     ##  ###       ####  ####         ## #####   ##       ##      ##    ##    ##    ##  ");
+        mvaddstr(7, 3, "    ##     ##                               ##   #      ###             ##      ####  ####    ");
+        mvaddstr(8, 3, "    ##     ##                           #####   ####                 #####                    ");
+        mvaddstr(9, 3, "           ##  ####                            #   ##               ##                        ");
+	mvaddstr(10,3, "                                                                    ##                        ");
+	mvaddstr(11,3, "                                                                     #####                    ");
+	
+        mvaddstr(15, 3, "SUDACHI");
+        mvaddstr(21, 3, "quit :[q]");
+
+        refresh();
+
+	while(1){
+		key = getch();
+		if(key == 'q') break;
+	}
+        return 0;
+}
+
+
 
 int main(void){
 	int h, w, key;
@@ -23,12 +97,11 @@ int main(void){
 	getmaxyx(stdscr, h, w);
 
 	while(1){
-		key = Title();
-		if(key == 's'){
-			Game();
-		}else if(key == 'q'){
-			break;
-		}
+		key = Title1();
+		if(key == 's') Game();
+		if(key == 'S') Title0();
+		if(key == 'q') break;
+
 	}
 	endwin();
 	return 0;
